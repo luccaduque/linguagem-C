c7b23298a4a63321f8797fec9da0cdd534a17bed
Lucca S. Duque <subsgg932@gmail.com>
2023-09-13 10:03:29 -0300

    commit

---
+++ arrays/ex6.c
@@ -0,0 +1,25 @@
+//Ler 5 elementos de um vetor A. No final, apresentar o total da soma de todos os elementos que sejam ímpares. 
+
+    #include <stdio.h>
+    int i;
+        int main () {
+            
+            int vetorA[5];
+
+                    for (i = 0; i <= 4; i++)
+                    {
+                        printf("\nInsira um valor inteiro ->  ");
+                        scanf("%d", &vetorA[i]);
+                    }
+                        int impar = 0;
+                        for ( i = 0; i <= 4; i++)
+                        {
+                            if (vetorA[i] % 2 != 0)
+                            {
+                                impar = vetorA[i] + impar;
+                            }
+                        }
+
+                        printf("\nA soma de todos os numeros impares ->  %d\n", impar);
+ return 0;                       
+        }
