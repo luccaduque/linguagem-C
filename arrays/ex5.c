9b2469541f2b03c554c9031ab1621d0bbbaba7f1
Lucca S. Duque <subsgg932@gmail.com>
2023-09-13 09:21:31 -0300

    commit

---
+++ arrays/ex5.c
@@ -0,0 +1,31 @@
+//Ler 15 elementos de um vetor A. Construir um vetor B do mesmo tipo, observando a seguinte lei de formação: “Todo elemento de B deverá ser o quadrado do elemento de A correspondente”. Apresentar o vetor B.
+
+    #include <stdio.h>
+
+        int main ()
+        {
+            int vetorA[15];
+            int vetorB[15];
+            int i;
+            int quad;
+
+                for (i = 0; i <= 14; i++)
+                {
+                    printf("\nValor VetorA ->  ");
+                    scanf("%d", &vetorA[i]);
+                }
+
+                
+                for ( i = 0; i <= 14; i++)
+                {
+                quad =  vetorA[i] * vetorA[i];
+                vetorB[i] = quad;
+                }
+
+                for (i = 0; i <= 14; i++)
+                {
+                    printf("\nPosicao %d vetorB -> %d\n", i+1, vetorB[i]);
+                }
+
+                
+        }
